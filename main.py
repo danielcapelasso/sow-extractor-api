a7ffd212a3ea87badb4eb0b1adb86ffec337eef7
danielcapelasso <daniel.capelasso@yalo.com>
2025-07-09 10:49:50 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
-from fastapi import FastAPI, UploadFile
+from fastapi import FastAPI, UploadFile, Header, HTTPException
 from parser import extract_sow_data
+import os
 
 app = FastAPI(title="SOW Extractor API")
 
+API_KEY = os.environ.get("API_KEY")  # lê do ambiente
+
 @app.post("/extract")
-async def extract(file: UploadFile):
+async def extract(
+    file: UploadFile,
+    authorization: str = Header(None)
+):
+    if authorization != f"Bearer {API_KEY}":
+        raise HTTPException(status_code=401, detail="Unauthorized")
+
     content = await file.read()
     result = extract_sow_data(content)
     return result
